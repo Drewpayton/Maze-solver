e1b6be02cf196bf44697d00595cfa04428ddb2ba
drewpayton <drewpayton18@gmail.com>
2024-06-01 09:21:06 -0500

    Added a Point and Line class that when called creates a line

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,7 @@
-from window import Window
+from window import *
 
 if __name__ == "__main__":
     win = Window(800, 600)
+    line = Line(Point(50, 50), Point(400, 400))
+    win.draw_line(line, "black")
     win.wait_for_close()
