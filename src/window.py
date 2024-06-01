8613b8b66f88a1814eb00b85e6405606417d61b1
drewpayton <drewpayton18@gmail.com>
2024-05-31 21:45:51 -0500

    added a main file that calls the window class to start the tkinter. Finished the window class to run and update

--- src/window.py
+++ src/window.py
@@ -1,11 +1,39 @@
 from tkinter import Tk, BOTH, Canvas
+import time
 
 class Window:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.root = Tk.Tk()
-        self.root.title("Tkinter Project")
-        self.canvas = Canvas(self.root, self.width, self.height, bg="yellow")
+        self.root = Tk()
+        self.root.title("Maze Solver")
+        self.canvas = Canvas(self.root, width=self.width, height=self.height, bg="pink")
         self.running = False
         self.canvas.pack()
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
+
+
+    def redraw(self):
+        self.root.update_idletasks()
+        self.root.update()
+
+    def wait_for_close(self):
+        self.running = True
+        while self.running:
+            self.redraw()
+            time.sleep(0.01)
+
+    def close(self):
+        self.running = False
+
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self):
+        pass
+    
+    def draw():
+        pass
