fdd43aff47607190f9f347b47625041da225f0f8
drewpayton <drewpayton18@gmail.com>
2024-06-04 09:33:04 -0500

    reworked the main to draw some cells, and to draw the move between each cell making it red or gray

--- src/cell.py
+++ src/cell.py
@@ -30,3 +30,17 @@ class Cell:
         if self.has_bottom_wall:
             bottom_line = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
             self._win.draw_line(bottom_line)
+
+    def draw_move(self, to_cell, undo=False):
+        self.center_x = (self._x1 + self._x2) // 2
+        self.center_y = (self._y1 + self._y2) // 2 
+
+        cell_center_x = (to_cell._x1 + to_cell._x2) // 2
+        cell_center_y = (to_cell._y1 + to_cell._y2) // 2
+
+        line = Line(Point(self.center_x, self.center_y), Point(cell_center_x, cell_center_y))
+        fill_color = "red" if not undo else "grey"
+        self._win.draw_line(line, fill_color)
+
+            
+            
